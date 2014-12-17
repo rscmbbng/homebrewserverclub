bb346ca276e89d291b7d0bc9bff7df9fa51702e8
root <root@homebrewserver.club>
2014-12-17 19:12:59 +0000

    HBSC Pi now also tweets its local ip

--- hbsc_locator.py
+++ hbsc_locator.py
@@ -1,31 +1,43 @@
 #!/usr/bin/python
 
 
-import twitter, os
+import twitter, os, socket, re, commands
 from requests import get
 
 #API Keys
-consumer_key = 
-consumer_secret =
-acces_token = 
-access_token_secret =
+consumer_key = ''
+consumer_secret = ''
+acces_token = ''
+access_token_secret = ''
 
 
 
-ip_file ='/var/www/ip' #change me to whatever you like
+ip_file = '/var/www/ip' #change me to whatever you like
+local_ip_file = '/var/www/local_ip'
 
 ip = get('http://api.ipify.org').text.encode('UTF-8') #get the current public IP adress
 
-if os.path.exists(ip_file):
-	old_ip = open(ip_file).read().strip('\n')
-	if ip != old_ip:
-		api = twitter.Api(consumer_key = consumer_key, consumer_secret=consumer_secret, access_token_key = acces_token, access_token_secret=access_token_secret)
-		api.PostUpdate('The HomeBrewServer.club is currently located at: '+str(ip))
-		os.system('echo "'+ip+'" > '+ip_file)
+#extract the ip address (or addresses) from ifconfig
+found_ips = []
+ips = re.findall( r'[0-9]+(?:\.[0-9]+){3}', commands.getoutput("/sbin/ifconfig"))
+for local_ip in ips:
+  if local_ip.startswith("255") or local_ip.startswith("127") or local_ip.endswith("255"):
+    continue
+  found_ips.append(local_ip)
 
+local_ip = found_ips[0]
+
+if os.path.exists(ip_file) and os.path.exists(local_ip_file):
+	old_ip = open(ip_file).read().strip('\n')
+	old_local_ip = open(local_ip_file).read().strip('\n')
+	if ip != old_ip or local_ip != old_local_ip:
+		api = twitter.Api(consumer_key = consumer_key, consumer_secret=consumer_secret, access_token_key = acces_token, access_token_secret=access_token_secret)
+		api.PostUpdate('The HomeBrewServer.club is currently located at: '+str(ip)+', '+str(local_ip))
+		os.system('echo "'+ip+'" > '+ip_file)
+		os.system('echo "'+local_ip+'" > '+local_ip_file)
 	else:
 		#In case the IP adress didn't change since last time, do nothing.
-		print('Still the same IP')
+		print('Still the same external IP')
 
 else:
-	print('This script assumes that you manually make', ip_file, 'to activate it')
+	print 'This script assumes that you manually make %s and %s to activate it' % (ip_file, local_ip_file)
